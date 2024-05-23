c1b6acea94abc44195c6d3d51bfec320435d9e4b
WineSteward <miguel2001carpinteiro@hotmail.com>
2024-05-23 16:23:41 +0100

    From Multas

---
+++ Views/FormMulta.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace PSI_DA_PL1_F.Views
+{
+    partial class FormMulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdicionarMultas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxMultas = new System.Windows.Forms.ListBox();
+            this.numericUpDownValor = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHoras = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHorasEdit = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownValorEdit = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnUpdateMulta = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRemoverMulta = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownValor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHoras)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHorasEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownValorEdit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.numericUpDownHorasEdit);
+            this.groupBox3.Controls.Add(this.numericUpDownValorEdit);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.btnUpdateMulta);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(13, 240);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(409, 204);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Área de Edição da Multa";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDownHoras);
+            this.groupBox1.Controls.Add(this.numericUpDownValor);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAdicionarMultas);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 223);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Área de Criação da Multa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor da Multa:";
+            // 
+            // btnAdicionarMultas
+            // 
+            this.btnAdicionarMultas.Location = new System.Drawing.Point(117, 181);
+            this.btnAdicionarMultas.Name = "btnAdicionarMultas";
+            this.btnAdicionarMultas.Size = new System.Drawing.Size(156, 23);
+            this.btnAdicionarMultas.TabIndex = 0;
+            this.btnAdicionarMultas.Text = "Adicionar Multa";
+            this.btnAdicionarMultas.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Número de Horas:";
+            // 
+            // listBoxMultas
+            // 
+            this.listBoxMultas.FormattingEnabled = true;
+            this.listBoxMultas.Location = new System.Drawing.Point(441, 12);
+            this.listBoxMultas.Name = "listBoxMultas";
+            this.listBoxMultas.Size = new System.Drawing.Size(347, 225);
+            this.listBoxMultas.TabIndex = 10;
+            // 
+            // numericUpDownValor
+            // 
+            this.numericUpDownValor.Location = new System.Drawing.Point(133, 69);
+            this.numericUpDownValor.Name = "numericUpDownValor";
+            this.numericUpDownValor.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownValor.TabIndex = 6;
+            // 
+            // numericUpDownHoras
+            // 
+            this.numericUpDownHoras.Location = new System.Drawing.Point(133, 131);
+            this.numericUpDownHoras.Name = "numericUpDownHoras";
+            this.numericUpDownHoras.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownHoras.TabIndex = 7;
+            // 
+            // numericUpDownHorasEdit
+            // 
+            this.numericUpDownHorasEdit.Location = new System.Drawing.Point(142, 105);
+            this.numericUpDownHorasEdit.Name = "numericUpDownHorasEdit";
+            this.numericUpDownHorasEdit.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownHorasEdit.TabIndex = 12;
+            // 
+            // numericUpDownValorEdit
+            // 
+            this.numericUpDownValorEdit.Location = new System.Drawing.Point(142, 43);
+            this.numericUpDownValorEdit.Name = "numericUpDownValorEdit";
+            this.numericUpDownValorEdit.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownValorEdit.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Valor da Multa:";
+            // 
+            // btnUpdateMulta
+            // 
+            this.btnUpdateMulta.Location = new System.Drawing.Point(126, 155);
+            this.btnUpdateMulta.Name = "btnUpdateMulta";
+            this.btnUpdateMulta.Size = new System.Drawing.Size(156, 23);
+            this.btnUpdateMulta.TabIndex = 8;
+            this.btnUpdateMulta.Text = "Atualizar Multa";
+            this.btnUpdateMulta.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Número de Horas:";
+            // 
+            // btnRemoverMulta
+            // 
+            this.btnRemoverMulta.Location = new System.Drawing.Point(562, 264);
+            this.btnRemoverMulta.Name = "btnRemoverMulta";
+            this.btnRemoverMulta.Size = new System.Drawing.Size(98, 55);
+            this.btnRemoverMulta.TabIndex = 13;
+            this.btnRemoverMulta.Text = "Remover Multa";
+            this.btnRemoverMulta.UseVisualStyleBackColor = true;
+            // 
+            // FormMulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemoverMulta);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBoxMultas);
+            this.Name = "FormMulta";
+            this.Text = "FormMulta";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownValor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHoras)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHorasEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownValorEdit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdicionarMultas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxMultas;
+        private System.Windows.Forms.NumericUpDown numericUpDownHoras;
+        private System.Windows.Forms.NumericUpDown numericUpDownValor;
+        private System.Windows.Forms.NumericUpDown numericUpDownHorasEdit;
+        private System.Windows.Forms.NumericUpDown numericUpDownValorEdit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUpdateMulta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRemoverMulta;
+    }
+}
